3b143dc7f0cb97371c00f089b6da5d8a8d16da6e
Sahat Yalkabov <sakhat@gmail.com>
2013-01-12 15:21:14 -0500

    added some pages

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
+    app.locals.pretty = true;
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -29,6 +30,12 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/step2', function(req, res) {
+  res.render('step2');
+});
+app.get('/results', function (req, res) {
+  res.render('results');
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
